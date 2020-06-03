65a2535365cc58d529cd6462d26d9654b58f5681
Shuhey <shuhey.dev@gmail.com>
2020-06-03 22:52:21 +0900

    Add project files.

---
+++ XFSeparatedDataTemplate/XFSeparatedDataTemplate/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFSeparatedDataTemplate.Models;
+
+namespace XFSeparatedDataTemplate
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Book> _books;
+        public ObservableCollection<Book> Books
+        {
+            get => _books;
+            set
+            {
+                _books = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+
+            var tempList = new List<Book>();
+            for(int i=0;i<20;i++)
+            {
+                tempList.Add(new Book
+                {
+                    AuthorName = $"Author{i}",
+                    Title = $"Title{i}",
+                    Description = $"Description{i}",
+                });
+            }
+
+            Books = new ObservableCollection<Book>(tempList);
+        }
+    }
+}
---
+++ XFSeparatedDataTemplate/XFSeparatedDataTemplate/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFSeparatedDataTemplate.Models
+{
+    public class Book
+    {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
